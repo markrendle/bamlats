24fa51f3c9d4ee9818cd2b04d7f86478abda361c
markrendle <mark@markrendle.net>
2016-01-19 14:07:14 +0000

    Add moment typings

--- src/typings/tsd.d.ts
+++ src/typings/tsd.d.ts
@@ -1,3 +1,5 @@
 /// <reference path="angularjs/angular.d.ts" />
 /// <reference path="jquery/jquery.d.ts" />
 /// <reference path="angular-ui-router/angular-ui-router.d.ts" />
+/// <reference path="moment/moment-node.d.ts" />
+/// <reference path="moment/moment.d.ts" />
